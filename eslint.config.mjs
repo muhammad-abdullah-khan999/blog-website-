4852d68b9690ce2cbe05536de106e921fca6a410
muhammad-abdullah-khan999 <iamgoodman222@gmail.com>
2025-01-02 13:53:30 +0500

    error fixed 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,21 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +29,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific ESLint rules here
+      "@typescript-eslint/no-unused-vars": "off", // Disable the no-unused-vars rule
+      "no-console": "off", // Disable the no-console rule
+    },
+  },
 ];
 
 export default eslintConfig;
